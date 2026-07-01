8acaa0ee45cf542c97bdaddbebc425e04a675753
yohannestakata <yohannestm@zagoal.com>
2026-07-01 02:29:43 -0400

    Use white native app background

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -70,7 +70,9 @@ function RootLayout() {
   }
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView
+      style={{ flex: 1, backgroundColor: colors.background }}
+    >
       <KeyboardProvider>
         <ClerkProvider
           publishableKey={clerkPublishableKey}
